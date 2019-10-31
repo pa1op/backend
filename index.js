a94435cb84a1c1d3512f56e9288718d261e99857
Antti Paloposki <antti.paloposki@eficode.com>
2019-10-31 21:22:44 +0100

    Add eslint tuning

--- index.js
+++ index.js
@@ -1,81 +1,82 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 
-morgan.token('body', (req, res) => JSON.stringify(req.body))
+const app = express();
 
-app.use(bodyParser.json())
-app.use(morgan(':method :url :status :response-time ms :body'))
-app.use(cors())
-app.use(express.static('build'))
+morgan.token('body', (req) => JSON.stringify(req.body));
+
+app.use(bodyParser.json());
+app.use(morgan(':method :url :status :response-time ms :body'));
+app.use(cors());
+app.use(express.static('build'));
 
 let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    }
-]
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4,
+  },
+];
 
 app.get('/info', (req, res) => {
-  res.end(`Phonebook has  info of ${persons.length} people\n${new Date()}`)
-})
+  res.end(`Phonebook has  info of ${persons.length} people\n${new Date()}`);
+});
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
+  res.json(persons);
+});
 
 app.post('/api/persons', (req, res) => {
-  const person = req.body
-  if (!person.name || !person.number) {
+  const person = req.body;
+  if (!person.name || !person.number) {
     return res.status(400).json({
-      error: 'request must contain name and number'
-    })
+      error: 'request must contain name and number',
+    });
   }
-  person.id = Math.floor(Math.random() * Math.floor(1000))
-  if (persons.map(person => person.name).includes(person.name)) {
+  person.id = Math.floor(Math.random() * Math.floor(1000));
+  if (persons.map((person) => person.name).includes(person.name)) {
     return res.status(400).json({
-      error: 'name must be unique'
-    })
+      error: 'name must be unique',
+    });
   }
-  persons = persons.concat(person)
-  res.status(201).json(person)
-})
+  persons = persons.concat(person);
+  return res.status(201).json(person);
+});
 
 app.get('/api/persons/:id', (req, res) => {
-  const person = persons.find((person) => person.id === Number(req.params.id))
-  person ? res.json(person) : res.status(404).end()
-})
+  const person = persons.find((person) => person.id === Number(req.params.id));
+  return person ? res.json(person) : res.status(404).end();
+});
 
 app.delete('/api/persons/:id', (req, res) => {
-  persons = persons.filter((person) => person.id !== Number(req.params.id))
-  res.status(204).end()
-})
+  persons = persons.filter((person) => person.id !== Number(req.params.id));
+  res.status(204).end();
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
